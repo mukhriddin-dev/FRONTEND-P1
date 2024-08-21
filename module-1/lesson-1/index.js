8439fa434efc1c594cfebaec32d77cdf55f479bc
mukhriddin-dev <khodievm@gmail.com>
2024-08-21 23:20:59 +0500

    LS 1 ready

---
+++ module-1/lesson-1/index.js
@@ -0,0 +1,111 @@
+// -> document.write("Bismillah!") documentga ma'lumot yozadi.
+// alert("Bismillah!");  browserda ogohlantirish oynasida yozib beradi.
+// confirm("Saytdan chiqasizmi ?"); browserda ogohlantirish oynasida so'rov beradi?
+// prompt("Ismingiz nima") browserda ogohlantirish oynasida matn qabul qiladi.
+
+
+
+// console.log('Bismillah Biz Js darslarini boshladik!'); 
+
+/*
+
+console.warn("Iltmos saytni qayta yuklang!");
+console.info(`Bugun 21-augst`);
+console.error("404 nimadur xato ketdi!");
+console.dir("./index.html");
+console.assert("assertion");
+console.table({name:"Sardor", age:21 , job:'programmer'});
+
+*/
+
+
+// Data types in JavaScript 
+
+/*
+
+1. String -  'Hello, World!' , "Assalamu alaykum!" , "2024.08.21" ,  `Salom`
+2. Number - 11 , -11 , 0.000001 , -0.23 , 7/8 , 0.1
+
+3.
+4.
+5.
+6.
+7.
+8
+
+
+*/
+
+
+// Varibales -> O'zgaruvchilar [ var  , let , const ]
+
+
+/*
+
+old version of JavaScript 
+
+var myname="Mirjon"  // declare
+    myname="Sardor" // re-assignment
+var myname="Mukhriddin" // re-declaration  
+
+var age=20;
+    age=25;
+var age=27;
+
+
+console.log(myname , age)
+
+
+
+// ES5+ 2009 2015
+
+let myName="Mirjon" // declare
+    myName="Sardor" // re-assignment
+// let myName="Mukhriddin"; // not re-declaration
+console.log(myName)
+
+
+
+// ES6+
+
+const myName="Mirjon" // declare
+// myName="Samandar" // re-assignment x
+// const myName="Sardor" // re-declaration x
+console.log(myName)
+
+*/
+
+// + , - , /  , * , ** , % , ++ , --
+
+// let a=2;
+// let b=4;
+
+// let result=a**b;
+
+// console.log(result)
+
+// let num=3;
+// num++; // increment
+// num--;    // decrement
+// console.log(num)  /// pre-incremented // pre-decremented
+
+console.log(11+"11") //  String -> '1111'
+console.log(11-"11") // Number -> 0
+console.log(11-"ab") // Number -> NaN = Not a Number
+console.log("122"*2) // Number -> 244
+console.log("90"/15) // Number -> 6
+console.log("f"*4)   // Number -> NaN
+
+console.log(typeof(11)) // number
+console.log(typeof("Salom")) // string
+console.log(typeof("a"*11)) // number
+
+
+
+
+
+
+
+
+
+
---
+++ module-1/lesson-1/readme.md
@@ -0,0 +1,19 @@
+### lesson-1
+
+- JavaScript syntax +
+- JavaScript variables +
+- JavaScript operators (+ , - , * , / , % , ++ , -- , **)
+- JavaScript input and output -
+- JavaScript naming ( variables , functions ) +
+- JavaScript Data types (extra) (STRING, NUBER)
+    
+```
+let myname="Mukhriddin" // 
+let myName="Mukhriddin" // camel case
+let MyName="Mukhriddin" // pascal case
+let my_name="Mukhriddin" // snake case
+
+let my-name="Mukhriddin" x // kebab-case
+
+```
+
