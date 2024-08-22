2933808f2cd3624bb075017e4932f911a71d339a
mukhriddin-dev <khodievm@gmail.com>
2024-08-22 20:33:26 +0500

    lesson v2

---
+++ module-1/lesson-2/index.js
@@ -0,0 +1,55 @@
+/* 
+
+let message="first message"; // string
+let year=2024;
+let app
+let nullish=null;
+
+let bool=false;
+let isTrue=true;
+
+let a=Symbol(1);   // unique identifier
+let b=Symbol(2);; // unique identifier
+
+const maxNum=12n;
+
+
+const person1={
+    name:"Jahongir",
+    age:23,
+    city:"Dhaka",
+    address:"Qatar",
+    isMarried:false,
+}
+
+const person2={
+    name:"Jahongir",
+    age:23,
+    city:"Dhaka",
+    address:"Qatar",
+    isMarried:false,
+}
+
+const arr=[1,"salom", true]
+
+console.log(person1==person2)
+
+*/
+
+// math operators: + , - ,  * ,  / , % , ++ , -- , **
+// logical operators: && , || , ! (not) 
+// == , === , != , !== , > , < , >= , <=
+
+// console.log( null || "" || "true" || undefined || ""  )
+
+// console.log(11==="11") // boolean value
+// console.log("S"=="s")
+// console.log(10!=="10")
+// console.log(12<10)
+// console.log(23>34)
+// console.log(11>=12)
+
+
+
+
+
---
+++ module-1/lesson-2/readme.md
@@ -0,0 +1,52 @@
+### Lessonon-2 
+
+— Javascript data types: [ boolean, null , undefined , Symbol , BigInteger , Object ] +
+- CallStack & Heap (Advanced) +
+- Truthy , Falthy
+- Opertaors (logical operators , || , && , == , === , !== , !=)
+- String
+
+### Data types in Javascript
+
+1. Primitive types
+
+- String +
+- Number +
+- Boolean + [ true , false]
+- undefined +
+- null   +
+- Symbol +
+- BigInt +
+
+2. Refrence (Non-Primitive) types 
+
+https://pashazade-nazar.medium.com/memory-life-cycle-heap-stack-call-stack-string-pool-bba2569524cb
+
+![alt text](image.png)
+
+- Object
+
+
+##  Truthy , Falthy
+
+### Truthy
+
+- 1 , 2, 3 ..., -2 , 1 , -0.111,
+- "salom" , "a" , "   ",
+- true 
+- [],
+- {}
+
+### Falthy
+
+- 0
+- ""
+- false
+- null
+- undefined
+
+
+
+
+
+
